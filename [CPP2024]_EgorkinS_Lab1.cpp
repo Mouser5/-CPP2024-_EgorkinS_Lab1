a77b70b7f553b4d735867b80295551557be9e5f2
Mouser5 <149000034+Mouser5@users.noreply.github.com>
2024-02-14 23:00:05 +0300

    Update [CPP2024]_EgorkinS_Lab1.cpp

--- [CPP2024]_EgorkinS_Lab1.cpp
+++ [CPP2024]_EgorkinS_Lab1.cpp
@@ -5,12 +5,23 @@ void Discriminant(const double a, const double b, const double c, double &D) {
     D = b * b - 4 * a * c;
 }
 
+void Roots(const double a, const double b, const double D,double & x1, double & x2) {
+    x1 = (-b + sqrt(D)) / (2 * a);
+    x2 = (-b - sqrt(D)) / (2 * a);
+};
 
 int main()
 {
-    double a = 0, b = 0, c = 0, D = 0, x = 0;
+    double a = 1, b = 4, c = 1, D = 0, x1 = 0, x2 = 0;
     //cin >> a >> b >> c;
     Discriminant(a, b, c, D);
-    
+    //cout << D;
+    if (D < 0) {
+        cout << "No roots";
+        return (0);
+    }
+    else {
+        Roots(a, b, D, x1, x2);
+    }
 
 }
