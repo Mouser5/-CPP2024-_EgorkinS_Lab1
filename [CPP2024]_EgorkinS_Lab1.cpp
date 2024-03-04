a99adf5039623f59ad79a7822de698cabded9a4d
Mouser5 <149000034+Mouser5@users.noreply.github.com>
2024-03-04 22:26:08 +0300

    Update [CPP2024]_EgorkinS_Lab1.cpp
    Add Discreminant

--- [CPP2024]_EgorkinS_Lab1.cpp
+++ [CPP2024]_EgorkinS_Lab1.cpp
@@ -5,9 +5,14 @@ void InputData(double &a, double &b, double &c){
   cin >> a >> b >> c;
 }
 
+void Discriminant(const double a, const double b, const double c, double &D) {
+    D = b * b - 4 * a * c;
+}
+
 int main()
 {
     double a = 0.0, b = 0.0, c = 0.0, D = 0.0, x1 = 0.0, x2 = 0.0;
     InputData(a,b,c);
+  Discriminant(a, b, c, D);
     return(0);
 }
