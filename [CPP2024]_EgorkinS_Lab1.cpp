05307c9f6eb89f6b156996dd4c9b73b7d63e2bfe
Mouser5 <149000034+Mouser5@users.noreply.github.com>
2024-03-04 22:34:31 +0300

    Update [CPP2024]_EgorkinS_Lab1.cpp

--- [CPP2024]_EgorkinS_Lab1.cpp
+++ [CPP2024]_EgorkinS_Lab1.cpp
@@ -20,7 +20,7 @@ int main()
     double a = 0.0, b = 0.0, c = 0.0, D = 0.0, x1 = 0.0, x2 = 0.0;
     InputData(a,b,c);
   Discriminant(a, b, c, D);   
-  if (D < 0) {
+  if (D < 0.0) {
         cout << "No roots";
         return (0);
     }
